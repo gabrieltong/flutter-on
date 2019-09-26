0c93e0b099b175989ce4a29d7152f13880b8f780
佟洋 <334812134@qq.com>
2019-09-26 17:24:33 +0800

    up

--- lib/page/schedule_stat.dart
+++ lib/page/schedule_stat.dart
@@ -1,14 +1,41 @@
 import 'package:f_on/component/layouts/default.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class ScheduleStatScreen extends StatefulWidget {
   @override
   _ScheduleStatScreenState createState() => _ScheduleStatScreenState();
 }
 
+class SalesData {
+  SalesData(this.year, this.sales);
+  final String year;
+  final double sales;
+}
+
 class _ScheduleStatScreenState extends State<ScheduleStatScreen> {
   @override
   Widget build(BuildContext context) {
-    return DefaultLayout(child: Container(child: Text("Schedule Stat")));
+    return DefaultLayout(
+        child: Column(children: <Widget>[
+      RaisedButton(child: Text("开始批改"), onPressed: () {}),
+      Card(
+          child: SfCartesianChart(
+              // Initialize category axis
+              primaryXAxis: CategoryAxis(),
+              series: <LineSeries<SalesData, String>>[
+            LineSeries<SalesData, String>(
+                // Bind data source
+                dataSource: <SalesData>[
+                  SalesData('Jan', 35),
+                  SalesData('Feb', 28),
+                  SalesData('Mar', 34),
+                  SalesData('Apr', 32),
+                  SalesData('May', 40)
+                ],
+                xValueMapper: (SalesData sales, _) => sales.year,
+                yValueMapper: (SalesData sales, _) => sales.sales)
+          ]))
+    ]));
   }
 }
